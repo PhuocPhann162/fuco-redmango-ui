25f61103b000d249e29d6aa3648fb647364d56a0
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-23 15:57:35 +0700

    add Redux Toolkit to project

---
+++ src/Storage/Redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { menuItemReducer } from "./menuItemSlice";
+
+const store = configureStore({
+  reducer: {
+    menuItemStore: menuItemReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
