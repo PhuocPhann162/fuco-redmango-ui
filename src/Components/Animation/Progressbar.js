f82847f3eb8d4bc08e26e39a7374b9d4173fe3ec
Trần Đông Đông <ongtrandong2@gmail.com>
2024-04-11 23:46:02 +0700

    Dong

--- src/Components/Animation/Progressbar.js
+++ src/Components/Animation/Progressbar.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./progressbar.css";
 
 const ProgressBar = ({ progress }) => {
-  document.documentElement.style.setProperty('--p', '0');
   return (
-    <div className="rol justify-content-b">
+    <div className="row">
       <div className="progress2 progress-moved">
         <div className="row justify-content-between">
-          <i className="col-xl-1 bi bi-star-fill icon " />
-          <div className="col-xl-2 loader" style={{ "--n": "1", "--f": "0" }} />
+          {/* <i className="col-xl-1 bi bi-star-fill icon " /> */}
+          <label className="col-xl-1 body icon"> VIP </label>
+          <div className="col-xl-2 loader"/>
         </div>
         <div className="progress-bar2" style={{ width: `${progress}%` }} />
       </div>
