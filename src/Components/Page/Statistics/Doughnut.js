9cdb38f0054e519ee5d64c04547e51c2f499fdb2
Trần Đông Đông <ongtrandong2@gmail.com>
2024-04-15 18:28:01 +0700

    Code with no data

---
+++ src/Components/Page/Statistics/Doughnut.js
@@ -0,0 +1,45 @@
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+ChartJS.register(
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function DoughnutChart() {
+  const labels = ["January", "February", "March", "April", "May", "June", "July"];
+  const data = {
+    labels: labels,
+    datasets: [{
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1
+    }]
+  };
+  const options = {};
+  return <Doughnut options={options} data={data} />;
+}
--- src/Pages/Statistics/Revenue.tsx
+++ src/Pages/Statistics/Revenue.tsx
@@ -1,15 +1,76 @@
 import React from "react";
+import { useState } from "react";
+import DailyChart from "../../Components/Page/Statistics/DailyRevenue";
+import MonthlyChart from "../../Components/Page/Statistics/MonthlyRevenue";
+import YearlyChart from "../../Components/Page/Statistics/YearlyRevenue";
+
+
+let decoration = require("../../Assets/Images/decoration_6.jpg");
 
 function Revenue() {
+  const [chart, setChart] = useState("DailyChart");
 
   return (
-    <div className="container">
-      <h1>Revenue</h1>
-      <div className="chart">
-        <canvas id="myChart" width="400" height="400"></canvas>
+    <div className="table p-4 fade-in">
+      <div className="d-flex align-items-center">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-auto">
+            <img
+              src={decoration}
+              alt="Decoration Header"
+              style={{
+                width: "100px",
+                height: "100px",
+                borderRadius: "50%",
+                marginRight: "20px",
+              }}
+            />
+          </div>
+          <div className="col">
+            <h1 className="text-success">Statistics</h1>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-12">
+        <div className="row justify-content-center">
+          <div className="col-xl-8">
+            {chart === "DailyChart" ? (
+              <DailyChart />
+            ) : chart === "MonthlyChart" ? (
+              <MonthlyChart />
+            ) : chart === "YearlyChart" ? (
+              <YearlyChart />
+            ) : null}
+          </div>
+          <div className="col-xl-2">
+            <h2 className="text-success">Revenue</h2>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="DailyChart" onChange={e => setChart(e.target.value)} checked={chart === "DailyChart"} />
+              <label className="form-check-label">
+                Daily Revenue
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="MonthlyChart" onChange={e => setChart(e.target.value)} checked={chart === "MonthlyChart"} />
+              <label className="form-check-label" >
+                Monthly Revenue
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" value="YearlyChart" onChange={e => setChart(e.target.value)} checked={chart === "YearlyChart"} />
+              <label className="form-check-label">
+                Yearly Revenue
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Revenue;
+
+{/* <LineChart /> */ }
+{/* <BarChart /> */ }
+{/* <DoughnutChart /> */ }
