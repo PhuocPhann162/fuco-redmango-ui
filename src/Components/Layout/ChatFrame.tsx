cab82cc743808001e92483df5366837ccd7ad66a
Trần Đông Đông <ongtrandong2@gmail.com>
2024-04-24 23:57:33 +0700

    Dong

--- src/Components/Layout/ChatFrame.tsx
+++ src/Components/Layout/ChatFrame.tsx
@@ -15,7 +15,7 @@ export default function ChatFrame({
         <div/>
         <div className="chat-input">
           <input type="text" placeholder="Type your message..." />
-          <button className="btn btn-primary" onClick={handleClose}>Close</button>
+          <button className="btn btn-primary rounder-circle" onClick={handleClose}>Close</button>
         </div>
       </div>
       )}
