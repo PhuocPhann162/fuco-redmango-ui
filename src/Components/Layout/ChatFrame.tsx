d0aaf34748f3a111d590968a9d52b81d57153e08
Trần Đông Đông <ongtrandong2@gmail.com>
2024-04-27 22:08:35 +0700

    fix frame

--- src/Components/Layout/ChatFrame.tsx
+++ src/Components/Layout/ChatFrame.tsx
@@ -6,18 +6,31 @@ type Props = {
 };
 
 export default function ChatFrame({
-  isShow,handleClose,
+  isShow, handleClose,
 }: Props) {
   return (
     <>
       {isShow && (
-        <div className="chat-frame">
-        <div/>
-        <div className="chat-input">
-          <input type="text" placeholder="..." />
-          <button className="btn btn-primary rounder-circle" onClick={handleClose}>Close</button>
+        <div className="d-flex justify-content-end">
+          <div className="chat-frame">
+            <div className="chat-header">
+              <div className="chat-header-title">Chat</div>
+              <div className="chat-header-icon">
+                <button className="btn btn-primary rounder-circle" onClick={handleClose}>X</button>
+              </div>
+            </div>
+            <div className="chat-body">
+              <div className="chat-message">
+                <div className="chat-message-sender">Sender</div>
+                <div className="chat-message-content">Content</div>
+              </div>
+            </div>
+            <div className="chat-input">
+              <input type="text" placeholder=".." />
+              <button className="btn btn-primary rounder-circle" onClick={handleClose}>Close</button>
+            </div>
+          </div>
         </div>
-      </div>
       )}
     </>
   );
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,14 +3,18 @@ import { MenuItemList } from "../Components/Page/Home";
 import { Banner } from "../Components/Page/Common";
 import { ChatButton } from "../Components/Animation";
 import { ChatFrame } from "../Components/Layout";
+import { set } from "date-fns";
 
 function Home() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isButtonShow, setIsButtonShow] = useState<boolean>(true);
   const handleOpen = () => {
     setIsOpen(true);
+    setIsButtonShow(false);
   };
   const handleClose = () => {
     setIsOpen(false);
+    setIsButtonShow(true);
   };
   return (
     <div className="container">
@@ -21,7 +25,7 @@ function Home() {
         </div>
       </div>
       <div className="z-999 fixed-bottom pb-5 pe-5">
-        <ChatButton handleOpenChatFrame={handleOpen}/>
+        <ChatButton isShow={isButtonShow} handleOpenChatFrame={handleOpen}/>
         <ChatFrame isShow={isOpen} handleClose={handleClose}/>
       </div>
     </div>
