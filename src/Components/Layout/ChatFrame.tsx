5a206128814c806c647a0be5aa9f2049cc9870c6
Trần Đông Đông <ongtrandong2@gmail.com>
2024-04-28 00:04:08 +0700

    Dong

--- src/Components/Layout/ChatFrame.tsx
+++ src/Components/Layout/ChatFrame.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React from "react";
 
 type Props = {
   isShow?: boolean;
@@ -6,28 +6,42 @@ type Props = {
 };
 
 export default function ChatFrame({
-  isShow, handleClose,
+  isShow,
+  handleClose,
 }: Props) {
   return (
     <>
       {isShow && (
         <div className="d-flex justify-content-end">
-          <div className="chat-frame">
-            <div className="chat-header">
-              <div className="chat-header-title">Chat</div>
-              <div className="chat-header-icon">
-                <button className="btn btn-primary rounder-circle" onClick={handleClose}>X</button>
+          <div className="row d-flex justify-content-center">
+            <div className="col-xl-12">
+              <div className="card" id="chat1" style={{ borderRadius: '15px', width: '300px', height: '500px' }}>
+                <div className="card-header d-flex justify-content-between align-items-center p-3 bg-info text-white border-bottom-0" style={{ borderTopLeftRadius: '15px', borderTopRightRadius: '15px' }}>
+                  <label className="mb-0 fw-bold">Live chat</label>
+                  <button type="button" className="btn-close" aria-label="Close" onClick={handleClose}></button>
+                </div>
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div>
+                    <div className="d-flex flex-row justify-content-start mb-4">
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp" alt="avatar 2" style={{ width: '45px', height: '100%' }} />
+                      <div className="p-3 ms-3" style={{ borderRadius: '15px', backgroundColor: 'rgba(57, 192, 237, .2)' }}>
+                        <label className="small">Hello Welcome, I'm chat bot, What do you want?</label>
+                      </div>
+                    </div>
+                    <div className="d-flex flex-row justify-content-end mb-4">
+                      <div className="p-3 me-3 border" style={{ borderRadius: '15px', backgroundColor: '#fbfbfb' }}>
+                        <label className="small">Thank you, I really like your product.</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="form-outline col-xl-8">
+                      <input className="form-control" id="textAreaExample"></input>
+                    </div>
+                    <button type="button" className="btn btn-primary col-xl-4">Send</button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="chat-body">
-              <div className="chat-message">
-                <div className="chat-message-sender">Sender</div>
-                <div className="chat-message-content">Content</div>
-              </div>
-            </div>
-            <div className="chat-input">
-              <input type="text" placeholder=".." />
-              <button className="btn btn-primary rounder-circle" onClick={handleClose}>Close</button>
             </div>
           </div>
         </div>
@@ -35,3 +49,4 @@ export default function ChatFrame({
     </>
   );
 }
+
